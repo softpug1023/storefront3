83a7ffe3f4ef622e0a13aa444068033511ec28c5
softpug1023 <jxie32@ucsc.edu>
2023-01-06 18:13:37 +0800

    learned til heroku

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,8 +1,15 @@
 import os
 from .common import *
+import dj_database_url
+
+
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["jiahongbuy-prod.herokuapp.com"]
+
+DATABASES = {
+    'default': dj_database_url.config() 
+}
